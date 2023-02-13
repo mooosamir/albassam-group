2d8269bfed00852c1ac5956d7fbc761a060e9508
bhaskar-acespritech <bhaskar.acespritech@gmail.com>
2023-02-13 14:16:19 +0530

    [IMP] : Improved batch payslip wizard excluding multi department

--- amcl_batch_payslip_extend/models/hr_batch_payslip.py
+++ amcl_batch_payslip_extend/models/hr_batch_payslip.py
@@ -7,15 +7,15 @@ from odoo.osv import expression
 class HrPayslipEmployees(models.TransientModel):
     _inherit = 'hr.payslip.employees'
 
-    exclude_department_id = fields.Many2one('hr.department', string='Exclude Department')
+    exclude_department_ids = fields.Many2many('hr.department', 'batch_emp_wizard_department_rel', 'batch_wizard_id', 'department_id', string='Exclude Department')
 
-    @api.depends('exclude_department_id')
+    @api.depends('exclude_department_ids')
     def _compute_employee_ids(self):
         res = super()._compute_employee_ids()
         for wizard in self:
             domain = wizard._get_available_contracts_domain()
-            if wizard.exclude_department_id:
-                department_ids = self.env['hr.department'].search([('id', 'child_of', self.exclude_department_id.id)])
+            if wizard.exclude_department_ids:
+                department_ids = self.env['hr.department'].search([('id', 'child_of', self.exclude_department_ids.ids)])
                 domain = expression.AND([
                     domain,
                     [('department_id', 'not in', department_ids.ids)]
