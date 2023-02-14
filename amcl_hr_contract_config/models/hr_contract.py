7d4048df92915140e6841f3fa814ab97195ff2ce
bhaskar-acespritech <bhaskar.acespritech@gmail.com>
2023-02-14 11:08:18 +0530

    [IMP] : Improved the wage calculation and basic

--- amcl_hr_contract_config/models/hr_contract.py
+++ amcl_hr_contract_config/models/hr_contract.py
@@ -25,12 +25,12 @@ class HRContract(models.Model):
             raise ValidationError(_('Required Element "%s" not found from Contract Elements.'%(contract_elem_id.name)))
 
 
-    @api.onchange('contract_element_line_ids')
-    def set_emp_wage_from_lines(self):
-        for line in self.contract_element_line_ids:
-            if line.contract_elem_conf_id.code == 'BASIC':
-                self.wage = line.amount
-                break
+    # @api.onchange('contract_element_line_ids')
+    # def set_emp_wage_from_lines(self):
+    #     for line in self.contract_element_line_ids:
+    #         if line.contract_elem_conf_id.code == 'BASIC':
+    #             self.wage = line.amount
+    #             break
 
     @api.constrains('contract_element_line_ids')
     def constrain_element_lines(self):
@@ -42,6 +42,7 @@ class HRContract(models.Model):
     @api.depends('wage', 'signon_bonus_amount', 'remote_allow', 'ticket_monthly', 'contract_element_line_ids')
     def _get_total_salary(self):
         for contract in self:
+            BASIC = contract.contract_element_line_ids.filtered(lambda line: line.code == 'BASIC').amount or 0.0
             ADD = contract.contract_element_line_ids.filtered(lambda line: line.code == 'ADD').amount or 0.0
             FOD = contract.contract_element_line_ids.filtered(lambda line: line.code == 'FOD').amount or 0.0
             HRA = contract.contract_element_line_ids.filtered(lambda line: line.code == 'HRA').amount or 0.0
@@ -57,11 +58,19 @@ class HRContract(models.Model):
             TA = contract.contract_element_line_ids.filtered(lambda line: line.code == 'TA').amount or 0.0
             UT_UT = contract.contract_element_line_ids.filtered(lambda line: line.code == 'UT/UT').amount or 0.0
 
-            contract.total_salary = contract.wage + \
-                                    ADD + FOD + INT + MFL + MOB + RPP + \
-                                    RSO + RTFI + SEN + SPC + SUP + UT_UT+ \
-                                    contract.signon_bonus_amount + \
-                                    HRA + TA + \
-                                    contract.other_allow + \
-                                    contract.remote_allow + \
-                                    contract.ticket_monthly
+            contract.wage = BASIC + \
+                            ADD + FOD + INT + MFL + MOB + RPP + \
+                            RSO + RTFI + SEN + SPC + SUP + UT_UT+ \
+                            contract.signon_bonus_amount + \
+                            HRA + TA + \
+                            contract.other_allow + \
+                            contract.remote_allow + \
+                            contract.ticket_monthly
+            contract.total_salary = BASIC + \
+                            ADD + FOD + INT + MFL + MOB + RPP + \
+                            RSO + RTFI + SEN + SPC + SUP + UT_UT+ \
+                            contract.signon_bonus_amount + \
+                            HRA + TA + \
+                            contract.other_allow + \
+                            contract.remote_allow + \
+                            contract.ticket_monthly
